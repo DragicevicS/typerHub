ac3199f7f64c2091ef8c4f4f75ecc552edcbe39a
DragicevicS <steffan95@live.com>
2023-09-01 21:19:43 +0200

    Move routing from main.jsx to App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import Level from "./pages/Level.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/app" element={<App />} />
-        <Route path="/level/:difficulty/:lesson" element={<Level />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
